c0e76438ac08453386549817fbd1f3c58d664e63
Caio Farias <caio4095@gmail.com>
2021-07-24 20:19:13 -0300

    Adicionado imagem de avatar para o user schema

--- src/db/migrations/20210722194247-create-users.js
+++ src/db/migrations/20210722194247-create-users.js
@@ -43,6 +43,10 @@ module.exports = {
           type: Sequelize.STRING,
           allowNull: false,
         },
+        avatar:{
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
         email: {
           type: Sequelize.STRING,
           allowNull: false,

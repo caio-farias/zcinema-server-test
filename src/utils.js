088071a2a464cd72bc6a9a35904ce3e810bbb41f
Caio Farias <caio4095@gmail.com>
2021-07-26 21:14:14 -0300

    utils method hotfix

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 module.exports = {
+  isDevEnviroment() {
+    return process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined
+  },
   generateBaseURL (){
-    const isDev = this.isDevEnviroment()
+    const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined
     return isDev ? `http://${process.env.HOST}:${process.env.PORT}`
       : `https://${process.env.HOST}`
   },
-  isDevEnviroment() {
-    return process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined
-  }
 }

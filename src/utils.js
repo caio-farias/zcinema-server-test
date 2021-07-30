80d24b97a3f2a0aac9ee456ce5363ab1621c3f16
Caio Farias <caio4095@gmail.com>
2021-07-29 23:16:02 -0300

    heroku hotfix

--- src/utils.js
+++ src/utils.js
@@ -2,11 +2,6 @@ require('dotenv/config')
 
 module.exports = {
   isDevEnviroment() {
-    return process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined
-  },
-  generateBaseURL (){
-    const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined
-    return isDev ? `http://${process.env.HOST}:${process.env.PORT}`
-      : `https://${process.env.HOST}`
+    return process.env.NODE_ENV === 'development'
   },
 }

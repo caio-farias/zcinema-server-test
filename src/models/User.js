3c4c0c1a35fa3b74d2765c037a6c49e355f0e8e4
Caio Farias <caio4095@gmail.com>
2021-07-29 23:40:47 -0300

    fix user mdoel

--- src/models/User.js
+++ src/models/User.js
@@ -24,26 +24,6 @@ class User extends Model {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      profile:{
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      avatar:{
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      avatar:{
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      profile:{
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      avatar:{
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
       email:      {
         type: DataTypes.STRING,
         allowNull: false,
